74a44c0fa793f0f64f74e4365b80db149479830f
malik-tillman <contact@maliktillman.com>
2020-11-08 13:21:19 -0500

    Did TODO (268, 12) - Completed `isDigit`

--- TokenStream.java
+++ TokenStream.java
@@ -266,6 +266,19 @@ public class TokenStream {
 
     private boolean isDigit(char c) {
         // TODO TO BE COMPLETED
+        switch (c) {
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case '0':
+                return true;
+        }
         return false;
     }
 
