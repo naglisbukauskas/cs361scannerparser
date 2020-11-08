44b19978d2397bec9253b06056b176d3a09d3cee
Naglis Bukauskas <naglisvbukauskas@gmail.com>
2020-11-08 12:43:41 -0500

    Editing ScannerDemo so that we can test changes to TokenStream. We also want to make sure that we can do pull requests properly.

--- ScannerDemo.java
+++ ScannerDemo.java
@@ -5,14 +5,16 @@
  */
 public class ScannerDemo {
 
-    private static String file1 = "C:\\Users\\cscharff\\eclipse-workspace\\ScannerSolution\\src\\prog1.jay";
+    private static String file1 = "D:\\Workspaces\\cs361scannnerparser\\src\\prog1.jay";
     private static int counter = 1;
 
     public static void main(String args[]) {
         TokenStream ts = new TokenStream(file1);
         System.out.println(file1);
+	Token tk = new Token();
         while (!ts.isEndofFile()) {
             // TO BE COMPLETED
+	    // tk = ts.nextToken();
             System.out.println("Token " + counter++ + " - Type: " + tk.getType() + " - Value: " + tk.getValue());
         }
     }
